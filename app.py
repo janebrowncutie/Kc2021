2d94869d72708e60ea068150deaa0ed4b07c6427
Kaida <janebrowncutie@gmail.com>
2021-08-02 13:34:24 +0000

    Typed at 9:34

--- app.py
+++ app.py
@@ -3,25 +3,25 @@
 from flask import Flask
 from flask import render_template
 from flask import request
-# from flask_pymongo import PyMongo
+from flask_pymongo import PyMongo
 
 
 # -- Initialization section --
 app = Flask(__name__)
 
-events = [
-        {"event":"First Day of Classes", "date":"2019-08-21"},
-        {"event":"Winter Break", "date":"2019-12-20"},
-        {"event":"Finals Begin", "date":"2019-12-01"}
-    ]
+# events = [
+#         {"event":"First Day of Classes", "date":"2019-08-21"},
+#         {"event":"Winter Break", "date":"2019-12-20"},
+#         {"event":"Finals Begin", "date":"2019-12-01"}
+#     ]
 
 # name of database
-# app.config['MONGO_DBNAME'] = 'database-name'
+app.config['MONGO_DBNAME'] = 'upperline'
 
 # URI of database
-# app.config['MONGO_URI'] = 'mongo-uri'
+app.config['MONGO_URI'] = 'mongodb+srv://admin:OQFZynT4zna3lPJe@cluster0.uftfv.mongodb.net/upperline?retryWrites=true&w=majority'
 
-# mongo = PyMongo(app)
+mongo = PyMongo(app)
 
 # -- Routes section --
 # INDEX
@@ -30,7 +30,8 @@ events = [
 @app.route('/index')
 
 def index():
-    return render_template('index.html', events = events)
+    user = mongo.db.users
+    return render_template('index.html', user = user)
 
 
 # CONNECT TO DB, ADD DATA
@@ -39,8 +40,48 @@ def index():
 
 def add():
     # connect to the database
+    user = mongo.db.users
 
     # insert new data
-
+    user.insert({'name' : "Kcaysha"})
+    
     # return a message to the user
-    return ""
+    return "Added user!"
+
+@app.route('/events', methods = ["GET", "POST"])
+def events():
+    if request.method == 'GET':
+        return "<h1> Trt again!</h1>"
+    else:
+        user_name = request.form['first_name']
+        user_date = request.form['date']
+
+        events = mongo.db.events
+        events.insert({'first_name': user_name, 'date': user_date})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# @app.route('/event')
+# def event():
+#     # collection = mongo.db.events
+#     # events = collection.find({})
+
+#     # return render_template('event.html', events = events)
+
+#      joy = mongo.db.users
+#      users = joy.find({'name' : "Kcaysha"})
+
+#      return render_template('event.html', users = users)
